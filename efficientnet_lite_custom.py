b1764a2907335d9a50f09e66fed565ffb232cb84
Srikanth Muralidharan <smuralid@cdr2604.int.cedar.computecanada.ca>
2025-05-15 12:38:17 -0700

    typo

--- efficientnet_lite_custom.py
+++ efficientnet_lite_custom.py
@@ -29,15 +29,9 @@ class EfficientNetLiteCustom(nn.Module):
         # Stem
         #model = timm.create_model('test_efficientnet.r160_in1k', pretrained=pretrain)
         #model = timm.create_model('mobilevit_xxs.cvnets_in1k', pretrained=pretrain, features_only=True)
-        model = timm.create_model('test_efficientnet_gn.r160_in1k', pretrained=pretrain, features_only=True)
-        old_weight = model.conv_stem.weight.data.mean(dim=1).unsqueeze(1)
-        new_layer = nn.Conv2d(1,24,kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-        with torch.no_grad():
-            new_layer.weight.copy_(old_weight)
-        model.conv_stem = new_layer
-        preprocess(model)
+        model = timm.create_model('test_efficientnet_gn.r160_in1k', pretrained=pretrai)
         self.model = model
         
     def forward(self, x):
         output = self.model(x)
-        return output[-3:-2]
+        return output
