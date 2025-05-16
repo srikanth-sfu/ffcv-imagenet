5a92e263036a8a85414130d300f86a31d6f1bd42
Srikanth Muralidharan <smuralid@cdr2485.int.cedar.computecanada.ca>
2025-05-15 17:33:18 -0700

    latest

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -1,7 +1,7 @@
 # Set the visible GPUs according to the `world_size` configuration parameter
 # Modify `data.in_memory` and `data.num_workers` based on your machine
-python train_imagenet.py --config-file rn18_configs/effnet_88_epochs.yaml \
-    --data.train_dataset=train_500_0.50_90.ffcv \
-    --data.val_dataset=val_500_0.50_90.ffcv \
-    --data.num_workers=1 --data.in_memory=1 \
+python train_imagenet.py --config-file rn50_configs/effnet_32_epochs.yaml \
+    --data.train_dataset=train_300_0.1_90.ffcv \
+    --data.val_dataset=val_300_0.1_90.ffcv \
+    --data.num_workers=32 --data.in_memory=1 \
     --logging.folder='.'
