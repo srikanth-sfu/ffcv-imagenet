11aca959db11be3ff54cf51d11262a8382c79d21
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-05-19 09:21:07 -0400

    distech

---
+++ efficientnet_distech.py
@@ -0,0 +1,61 @@
+import math
+
+import torch
+import torch.functional as F
+import torch.utils.model_zoo as model_zoo
+from torch import nn
+import timm
+from timm.models.layers import BatchNormAct2d, ConvBnAct
+from timm.models._efficientnet_blocks import EdgeResidual
+
+def preprocess(module, replacement=nn.ReLU(inplace=True), parent=True):
+    for name, child in module.named_children():
+        if isinstance(child, nn.SiLU):
+            setattr(module, name, replacement)
+        else:
+            preprocess(child, replacement, parent=False)
+    # if parent:
+    #     module.classifier = torch.nn.Identity()
+    #     module.global_pool = torch.nn.Identity()
+    #     module.conv_head = torch.nn.Identity()
+    #     module.bn2 = torch.nn.Identity()
+
+
+class EfficientNetLiteCustom(nn.Module):
+    def __init__(
+        self, pretrain
+    ):
+        super(EfficientNetLiteCustom, self).__init__()
+        
+        # Stem
+        #model = timm.create_model('test_efficientnet.r160_in1k', pretrained=pretrain)
+        #model = timm.create_model('mobilevit_xxs.cvnets_in1k', pretrained=pretrain, features_only=True)
+        model = timm.create_model('test_efficientnet_gn.r160_in1k', pretrained=pretrain)
+        self.model = model
+        self.preprocess()
+       
+    
+    def preprocess(self):
+        model = self.model
+        model.conv_stem = torch.nn.Conv2d(1,12, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+        model.bn1 = BatchNormAct2d(12)
+        model.blocks[0][0].conv = torch.nn.Conv2d(12, 12, kernel_size=(3, 3), stride=(1,1), padding=(1, 1), bias=False, groups=12)
+        model.blocks[0][0].bn1 = BatchNormAct2d(12)
+
+        model.blocks[1][0].conv_exp = nn.Conv2d(12, 12, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False, groups=12)
+        model.blocks[1][0].bn1 = BatchNormAct2d(12)
+        model.blocks[1][0].conv_pwl = nn.Conv2d(12, 20, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        model.blocks[1][0].bn2 = BatchNormAct2d(20)
+
+        model.blocks[2][0].conv_exp = nn.Conv2d(20, 20, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False, groups=20)
+        model.blocks[2][0].bn1 = BatchNormAct2d(20)
+        model.blocks[2][0].conv_pwl = nn.Conv2d(20, 20, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False, groups=20)
+        model.blocks[2][0].bn2 = BatchNormAct2d(20)
+        
+        model.blocks[3][0].conv_pw = nn.Conv2d(20, 128, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        self.model = model
+     
+    def forward(self, x):
+        x = x.mean(dim=1).unsqueeze(1)
+        output = self.model(x)
+        return output
