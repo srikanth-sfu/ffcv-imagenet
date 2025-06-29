66c1c898bbaec9dc078822bd0a97e6d331a62368
Srikanth Muralidharan <smuralid@narval2.narval.calcul.quebec>
2025-06-29 11:39:07 -0400

    latest

--- grid_search.py
+++ grid_search.py
@@ -2,8 +2,8 @@ import numpy as np
 from itertools import product
 import os
 
-alpha_range = np.linspace(1.1, 1.3, 6) + 0.3 # e.g. [1.1, 1.2, 1.3]
-beta_range = np.linspace(0.85, 1.15, 6) + 0.3  # e.g. [1.05, 1.1, 1.15]
+alpha_range = np.linspace(1.1, 1.3, 8) + 0.3 # e.g. [1.1, 1.2, 1.3]
+beta_range = np.linspace(0.85, 1.15, 5) + 0.3  # e.g. [1.05, 1.1, 1.15]
 
 
 candidates = []
