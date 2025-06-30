2c75fd7d37b3cbbec0888decde85af2f58b25954
srikanth-sfu <srikanth.srikanth18@gmail.com>
2025-06-29 22:28:48 -0400

    taylor7

--- train_imagenet.sh
+++ train_imagenet.sh
@@ -1,8 +1,8 @@
 # Set the visible GPUs according to the `world_size` configuration parameter
 # Modify `data.in_memory` and `data.num_workers` based on your machine
 python train_imagenet.py --config-file rn50_configs/tinynet_tmpl.yaml \
-    --data.train_dataset=train_400_0.10_90.ffcv \
-    --data.val_dataset=val_400_0.10_90.ffcv \
+    --data.train_dataset=../data/train_400_0.10_90.ffcv \
+    --data.val_dataset=../data/val_400_0.10_90.ffcv \
     --data.num_workers=12 --data.in_memory=1 \
     --model.alpha=$1 --model.beta=$2 \
-    --logging.folder='/scratch/smuralid/gridsearch_logs/'
+    --logging.folder='gridsearch_logs/'
